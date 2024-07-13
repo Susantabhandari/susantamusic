faf96e546b4b1c4168b06ab5254c5206ca66fb48
Susanta Bhandari <89909878+Susantabhandari@users.noreply.github.com>
2024-07-13 15:49:21 +0545

    Update logging.py

--- VenomX/logging.py
+++ VenomX/logging.py
@@ -15,5 +15,6 @@ logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
+
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
